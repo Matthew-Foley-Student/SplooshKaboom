434355c70b3174b84006a016968bc1564e15f2e9
Matthew-Foley-Student <foleymat000@gmail.com>
2025-11-21 22:21:21 -0800

    Add files via upload

---
+++ Week 3/SplooshKaboom/Form1.cs
@@ -0,0 +1,16 @@
+/*
+ * Matthew Foley
+ * CST-201-O500
+ * December 2025
+ * BattleShips
+ */
+namespace SplooshKaboom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
